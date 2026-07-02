4bfa5ed4d08dda18894c57f09ee82ddbee60fc7e
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-07-02 15:21:57 -0400

    refactor: Completed experience section

--- frontend/src/components/ExperienceSection/index.tsx
+++ frontend/src/components/ExperienceSection/index.tsx
@@ -7,9 +7,10 @@ const EXPERIENCES = [
     roles: [
       {
         title: "Software Solutions Engineer",
-        period: "",
-        location: "",
-        description: "",
+        period: "June 2026 - Present",
+        location: "Waltham, MA, USA",
+        description:
+          "Ill update this in a few months when I can more accurately talk about my work here :)",
       },
     ],
   },
@@ -17,16 +18,18 @@ const EXPERIENCES = [
     company: "Northeastern University Khoury College of Computer Sciences",
     roles: [
       {
-        title: "Teaching Assistant",
-        period: "",
-        location: "",
-        description: "",
+        title: "Teaching Assistant for CS3000: Data Structures & Algorithms",
+        period: "May - July 2026",
+        location: "Boston, MA, USA",
+        description:
+          "Worked alongside a team of 10+ teaching assistants to support a summer session of CS3000, a core course in the computer science curriculum. Responsibilities included holding office hours for  135+ students, lead recitation sessions for 25+ students, held individualized tutoring sessions, and assited with overall grading, material proofreading, and course management.",
       },
       {
         title: "Peer Mentor",
-        period: "",
-        location: "",
-        description: "",
+        period: "August - May 2026",
+        location: "Boston, MA, USA",
+        description:
+          "Worked with a team of 20+ peer mentors to support first and second year students through the co-op application process. Lead career development workshops and one-on-one sessions to help students with resume building, interview preparation, and job search strategies.",
       },
     ],
   },
@@ -35,8 +38,8 @@ const EXPERIENCES = [
     roles: [
       {
         title: "Software Engineer Intern",
-        period: "May – Aug 2025",
-        location: "Portland, ME",
+        period: "May – August 2025",
+        location: "Portland, ME, USA",
         description:
           "R&D software development focused on building testing automation tools for the company's C++ embedded systems. Created documentation and maintenance systems for embedded software and other internal projects.",
       },
@@ -47,10 +50,10 @@ const EXPERIENCES = [
     roles: [
       {
         title: "Data Architecture Engineer Co-Op",
-        period: "Aug – Dec 2024",
+        period: "August - December 2024",
         location: "Bogotá, Colombia",
         description:
-          "Collaborated with the engineering team to develop data infrastructure and pipelines, modernizing the company's information processing and management systems.",
+          "Collaborated with the engineering team to develop data infrastructure and pipelines, modernizing the company's financial information processing and management systems.",
       },
     ],
   },
